c7321801092f2001d3eddb5bf24212483224a5bc
seungyeopme <seungyeop.me.dev@gmail.com>
2022-06-14 22:44:51 +0900

    first commit

---
+++ CafeApp/CafeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CafeApp
+//
+//  Created by seungyeop on 2022/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
